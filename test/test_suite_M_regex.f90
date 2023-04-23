9b60b6b7be33c542c3035df90752cd3a43c12ace
John S. Urban <urbanjost@comcast.net>
2023-04-23 18:04:15 -0400

    testing

--- test/test_suite_M_regex.f90
+++ test/test_suite_M_regex.f90
@@ -1,7 +1,7 @@
 program test_program
 use M_regex, only: regex_type, regcomp, regexec, regmatch, regfree, regerror
 use M_framework__verify, only: unit_check, unit_check_good, unit_check_bad, unit_check_done, unit_check_start, unit_check_level
-use M_framework__verify, only : unit_check_command, unit_check_keep_going, unit_check_level, unit_check_stop
+use M_framework__verify, only : unit_check_level, unit_check_stop
 implicit none
  
 type(regex_type)             :: regex
@@ -23,8 +23,6 @@ integer                      :: i
       write(*,*) 'match="',regmatch(i,input_line,matches),'"'
    ENDDO
 
-   unit_check_command=''
-   unit_check_keep_going=.true.
 !  unit_check_level=1
    unit_check_level=0
    call regfree(regex)
